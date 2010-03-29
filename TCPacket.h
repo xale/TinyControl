38c7d37b542ea70d53bfb2f6fa129cb977d66347
Alex Heinz <xale@acm.jhu.edu>
2010-03-29 15:03:58 -0400

    Added a header file with some constants for packet field widths.

---
+++ TCPacket.h
@@ -0,0 +1,14 @@
+// Packet.h
+// Created by xale on 3/20/10
+//
+// Header file with constants and definitions related to packets as specified by the TinyControl protocol.
+//
+
+// All field widths are measured in bytes
+#define SEQ_NUM_FIELD_WIDTH		4
+#define TIMESTAMP_FIELD_WIDTH	4
+#define RTT_FIELD_WIDTH			4
+#define MAX_DATA_PAYLOAD_SIZE	1000
+#define ELAPSED_T_FIELD_WIDTH	4
+#define RECV_RATE_FIELD_WIDTH	4
+#define LOSS_RATE_FIELD_WIDTH	4
