d657135c9d6e95a9a0b1bb7d4e32e2e158b97906
Alex Heinz <xale@acm.jhu.edu>
2010-03-29 16:23:17 -0400

    More in the TCServer header file.

--- TCServer.h
+++ TCServer.h
@@ -4,22 +4,36 @@
 // Header file for the server version of the TinyControl protocol socket.
 //
 
-typedef int UDPSocket;
+#include <pthread.h>
+#include <stdint.h>
+#include <netinet/in.h>
+
+#ifndef	TCSERVER_H
+#define	TCSERVER_H
+
+typedef int udp_socket;
+typedef struct sockaddr_in inet_socket_address;
 
 typedef struct _TCServerSocket
 {
-	UDPSocket _sock;
-	// FIXME: more
+	udp_socket _sock;
+	pthread_t _readThread;
+	inet_socket_address _socketAddress;
 } TCServerSocket;
 
 typedef TCServerSocket* TCServerSocketRef;
 
 // Creates a new TinyControl server socket
-// FIXME: arguments
-TCServerSocketRef TCServerSocketCreate();
+TCServerSocketRef TCServerSocketCreate(const char* address, uint16_t listenPort);
 
 // Cleans up and frees a TinyControl server socket
 void TCServerSocketDestroy(TCServerSocketRef socket);
 
 // Sends the specified data over a TinyControl server socket
 void TCServerSocketSendBytes(TCServerSocketRef socket, const char* data);
+
+// Accessors for data about a TinyControl server socket
+uint32_t TCServerSocketGetAddress(TCServerSocketRef socket);
+uint16_t TCServerSocketGetPort(TCServerSocketRef socket);
+
+#endif	// TCSERVER_H
