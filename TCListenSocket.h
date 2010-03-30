9cd89eda6b30e4eb31f415b5af6805e67fb5f26f
Alex Heinz <xale@acm.jhu.edu>
2010-03-30 19:49:25 -0400

    Added a timeout argument to TCListenSocketAccept().

--- TCListenSocket.h
+++ TCListenSocket.h
@@ -1,10 +1,11 @@
 // TCListenSocket.h
 // Created by xale on 3/29/10
 //
-// Header file for the server-listener version of the TinyControl protocol socket.
+// Header file for a UDP listener socket that creates and establishes connections over TinyControl server sockets.
 //
 
 #include <stdint.h>
+#include <sys/time.h>
 #include "TCServerSocket.h"
 #include "TCTypes.h"
 
@@ -24,7 +25,7 @@ TCListenSocketRef TCListenSocketCreate(uint16_t listenPort);
 // Closes, cleans up and frees a TinyControl listen socket
 void TCListenSocketDestory(TCListenSocketRef listenSocket);
 
-// Blocks on accepting an incoming connection on the specified TinyControl listen socket, returning a new TinyControl server socket when a connection is established.
-TCServerSocketRef TCListenSocketAccept(TCListenSocketRef listenSocket);
+// Blocks on accepting an incoming connection on the specified TinyControl listen socket, returning a new TinyControl server socket when a connection is established, or NULL after the specified timeout
+TCServerSocketRef TCListenSocketAccept(TCListenSocketRef listenSocket, const struct timeval timeout);
 
 #endif // TCLISTENSOCKET_H
