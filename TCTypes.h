6aff346e0b1fc552235631626673ef0eff3e1f41
Alex Heinz <xale@acm.jhu.edu>
2010-03-30 12:39:38 -0400

    Added a header file for useful global typedefs and constants.

---
+++ TCTypes.h
@@ -0,0 +1,15 @@
+// TCTypes.h
+// Created by xale on 3/30/10
+//
+// Header file with typedefs and defines for TinyControl protocol.
+//
+
+#include <netinet/in.h>
+
+#ifndef TCTYPES_H
+#define TCTYPES_H
+
+typedef int socket_fd;
+typedef struct sockaddr_in inet_socket_address;
+
+#endif // TCTYPES_H
