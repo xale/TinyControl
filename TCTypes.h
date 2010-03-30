decf3457ebf11b3dab3e97723839410f41a4ca4e
Alex Heinz <xale@acm.jhu.edu>
2010-03-30 17:22:29 -0400

    Applied changes to TCTypes.h from the server branch.

--- TCTypes.h
+++ TCTypes.h
@@ -9,7 +9,14 @@
 #ifndef TCTYPES_H
 #define TCTYPES_H
 
-typedef int socket_fd;
-typedef struct sockaddr_in inet_socket_address;
+typedef struct addrinfo				inet_address_info;
+typedef struct sockaddr_in			inet_socket_address;
+typedef int 						socket_fd;
+
+const struct timeval TC_HANDSHAKE_TIMEOUT = {5, 0}; // 5 seconds
+const size_t TC_HANDSHAKE_BUFFER_SIZE =		16;
+const char* TC_HANDSHAKE_SYN_MSG =			"TC_SYN";
+const char* TC_HANDSHAKE_SYNACK_MSG =		"TC_SYNACK";
+const char* TC_HANDSHAKE_ACK_MSG =			"TC_ACK";
 
 #endif // TCTYPES_H
