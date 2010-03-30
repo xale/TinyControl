c66384e10030271876aa4e3fc0241804c30406ee
Alex Heinz <xale@acm.jhu.edu>
2010-03-30 19:29:09 -0400

    Wrote implementation of TCServerSocketCreate(); various corrections to TCServerSocketConnect(); argument name changes.

--- TCServerSocket.c
+++ TCServerSocket.c
@@ -4,56 +4,75 @@
 // Implementation file for TCServerSocket
 //
 
-#include <string.h> // memset()
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "TCServerSocket.h"
 
 // Private "methods"
 // Attempts to perform the second and third stages of a connection handshake; returns a socket file descriptor connected to the client if successful, or -1 in the event of an error.
 socket_fd TCServerSocketConnect(inet_socket_address connectAddress);
+// Manages the congestion monitoring/flow control of the specified socket; run as a separate thread.
+void* TCServerSocketFlowControl(void* serverSocket);
 
 TCServerSocketRef TCServerSocketCreate(inet_socket_address connectAddress)
 {
 	// Attempt to finalize the connection with the client
-	socket_fd socket = TCServerSocketConnect(connectAddress);
+	socket_fd sock = TCServerSocketConnect(connectAddress);
 	
 	// If the connection fails, bail
-	if (socket < 0)
+	if (sock < 0)
 		return NULL;
 	
+	// Create the server socket object
+	TCServerSocketRef serverSocket = malloc(sizeof(TCServerSocket));
+	if (serverSocket == NULL)
+		return NULL;
+	serverSocket->_sock = sock;
+	serverSocket->_remoteAddress = connectAddress;
 	
+	// Create a thread to handle reading congestion feedback information from the client
+	int rc = pthread_create(&(serverSocket->_readThread), NULL, TCServerSocketFlowControl, (void*)serverSocket);
+	if (rc != 0)
+		return NULL;
+	
+	// If everything looks good so far, return the new socket, ready for writing
+	return serverSocket;
 }
 
-socket_fd TCServerSockectConnect(inet_socket_address
+socket_fd TCServerSocketConnect(inet_socket_address connectAddress)
 {
 	// Attempt to create a UDP socket
-	socket_fd socket = socket(AF_INET, SOCK_DGRAM, 0);
-	if (socket < 0)
+	socket_fd sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0)
 		return -1;
 	
 	// Send a reply to the client host's connection request (automatically binds local socket)
-	sendto(socket, TC_HANDSHAKE_SYNACK_MSG, sizeof(TC_HANDSHAKE_SYNACK_MSG), 0, connectAddress, sizeof(connectAddress));
+	sendto(sock, TC_HANDSHAKE_SYNACK_MSG, sizeof(TC_HANDSHAKE_SYNACK_MSG), 0, (struct sockaddr*)&connectAddress, sizeof(connectAddress));
 	
 	// Create read a file-descriptor-set for select()ing on the socket
-	fd_set readFDs, errorFDs;
+	fd_set readFDs;
 	FD_ZERO(&readFDs);
 	
 	// Add the socket to the set
-	FD_SET(socket, &readFDs);
+	FD_SET(sock, &readFDs);
 	
 	// Select on the socket, waiting for the client to ACK the connection
-	switch (select((int)socket + 1, &readFDs, NULL, NULL, &TC_HANDSHAKE_TIMEOUT))
+	struct timeval timeout = TC_HANDSHAKE_TIMEOUT;
+	switch (select((int)sock + 1, &readFDs, NULL, NULL, &timeout))
 	{
-		case SOCKET_ERROR:
+		case -1:
 		case 0:
 			// Socket error, or connection timed out; connection failed
 			return -1;
 		
 		default:
-			if (FD_ISSET(socket, &readFDs) != 0)
+			if (FD_ISSET(sock, &readFDs) != 0)
 			{
 				// Read the client's message
 				char readBuffer[TC_HANDSHAKE_BUFFER_SIZE + 1];
-				size_t bytesRead = recv(socket, readBuffer, TC_HANDSHAKE_BUFFER_SIZE, 0);
+				size_t bytesRead = recv(sock, readBuffer, TC_HANDSHAKE_BUFFER_SIZE, 0);
 				readBuffer[bytesRead] = 0;
 				
 				// Check that the message is an ACK
@@ -73,25 +92,32 @@ socket_fd TCServerSockectConnect(inet_socket_address
 	}
 	
 	// Return the connected socket
-	return socket;
+	return sock;
 }
 
-void TCServerSocketDestroy(TCServerSocketRef socket)
+void TCServerSocketDestroy(TCServerSocketRef serverSocket)
 {
 	// FIXME: WRITEME
 }
 
-void TCServerSocketSendBytes(TCServerSocketRef socket, const char* data)
+void TCServerSocketSendBytes(TCServerSocketRef serverSocket, const char* data)
 {
 	// FIXME: WRITEME
 }
 
-uint32_t TCServerSocketGetRemoteAddress(TCServerSocketRef socket)
+void* TCServerSocketFlowControl(void* serverSocket)
 {
-	return socket->_remoteAddress.s_addr;
+	// FIXME: WRITEME
+	
+	pthread_exit(NULL);
 }
 
-uint16_t TCServerSocketGetRemotePort(TCServerSocketRef socket)
+uint32_t TCServerSocketGetRemoteAddress(TCServerSocketRef serverSocket)
 {
-	return socket->_remoteAddress.sin_port;
+	return serverSocket->_remoteAddress.sin_addr.s_addr;
+}
+
+uint16_t TCServerSocketGetRemotePort(TCServerSocketRef serverSocket)
+{
+	return serverSocket->_remoteAddress.sin_port;
 }
