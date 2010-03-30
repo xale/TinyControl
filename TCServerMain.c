a822876f6aee2b830893e0bfa59c8783e5bcaba1
Alex Heinz <xale@acm.jhu.edu>
2010-03-30 19:32:25 -0400

    Added a skeletal file for the server main().

---
+++ TCServerMain.c
@@ -0,0 +1,15 @@
+// TCServerMain.c
+// Created by xale on 3/30/10
+//
+// Main driver program for the TinyControl protocol server implementation
+//
+
+#include <stdlib.h>
+#include "TCServerSocket.h"
+
+int main(int argc, char** argv)
+{
+	// FIXME: WRITEME
+	
+	return EXIT_SUCCESS;
+}
