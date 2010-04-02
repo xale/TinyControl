6552c6579cbcf69d7ff019e5729a46ea405cc225
Alex Heinz <xale@acm.jhu.edu>
2010-04-02 16:30:55 -0400

    Redefined the value of the handshake SYNACK message to avoid having a common prefix with the handshake SYN message.

--- TCTypes.c
+++ TCTypes.c
@@ -9,5 +9,5 @@
 const struct timeval TC_HANDSHAKE_TIMEOUT =	{5, 0}; // 5 seconds
 const size_t TC_HANDSHAKE_BUFFER_SIZE =		16;
 const char* TC_HANDSHAKE_SYN_MSG =			"TC_SYN";
-const char* TC_HANDSHAKE_SYNACK_MSG =		"TC_SYNACK";
+const char* TC_HANDSHAKE_SYNACK_MSG =		"TC_SAK";
 const char* TC_HANDSHAKE_ACK_MSG =			"TC_ACK";
