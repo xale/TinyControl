177913632c18d6d2cf9d495400db53aa292f2ff4
Alex Heinz <xale@acm.jhu.edu>
2010-04-02 14:20:21 -0400

    Changed TCListenSocketCreate() to accept the port number as a string rather than a short, and wrote the function's implementation.

--- TCListenSocket.c
+++ TCListenSocket.c
@@ -4,17 +4,53 @@
 // Implementation file for a UDP listener socket that creates TinyControl server sockets.
 //
 
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
 #include "TCListenSocket.h"
 
-TCListenSocketRef TCListenSocketCreate(uint16_t listenPort)
+TCListenSocketRef TCListenSocketCreate(const char* listenPort)
 {
-	// FIXME: WRITEME
-	return NULL;
+	// Look up the necessary address information to create a local listen socket
+	inet_address_info addressHints;
+	inet_address_info_list addressResults;
+	memset(&addressHints, 0, sizeof(addressHints));
+	addressHints.ai_family = AF_INET;
+	addressHints.ai_socktype = SOCK_DGRAM;
+	addressHints.ai_flags = AI_PASSIVE; // autofill local IP address
+	if (getaddrinfo(NULL, listenPort, &addressHints, &addressResults) != 0)
+		return NULL;
+	
+	// Keep the first of the address lookup results
+	inet_address_info result = *addressResults;
+	result.ai_next = NULL;
+	
+	// Free the remaining results
+	freeaddrinfo(addressResults);
+	
+	// Create a new socket
+	socket_fd newSocket = socket(result.ai_family, result.ai_socktype, result.ai_protocol);
+	
+	// Bind the socket to the specified port
+	if (bind(newSocket, result.ai_addr, result.ai_addrlen) != 0)
+		return NULL;
+	
+	// Create the listen socket wrapper struct
+	TCListenSocketRef listenSocket = malloc(sizeof(TCListenSocket));
+	if (listenSocket == NULL)
+		return NULL;
+	
+	// Wrap the socket and return
+	listenSocket->sock = newSocket;
+	return listenSocket;
 }
 
 void TCListenSocketDestory(TCListenSocketRef listenSocket)
 {
-	// FIXME: WRITEME
+	// Free the socket wrapper struct
+	free(listenSocket);
 }
 
 TCServerSocketRef TCListenSocketAccept(TCListenSocketRef listenSocket, const struct timeval timeout)
