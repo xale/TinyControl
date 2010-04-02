e3fee88fb1a6cdb121afb94daf3ef51a003500eb
Alex Heinz <xale@acm.jhu.edu>
2010-04-02 19:22:08 -0400

    Wrote a very simple main implementation.

--- TCServerMain.c
+++ TCServerMain.c
@@ -5,11 +5,45 @@
 //
 
 #include <stdlib.h>
+#include <stdio.h>
+#include "TCListenSocket.h"
 #include "TCServerSocket.h"
+
+void usage();
 
 int main(int argc, char** argv)
 {
-	// FIXME: WRITEME
+	if (argc != 2)
+	{
+		usage();
+		return EXIT_FAILURE;
+	}
+	
+	// Create a listen socket
+	printf("DEBUG: creating listenSocket on port %s\n", argv[1]);
+	TCListenSocketRef listenSocket = TCListenSocketCreate(argv[1]);
+	
+	// Listen for incoming connections
+	struct timeval listenTimeout = {5, 0};
+	TCServerSocketRef serverSocket;
+	while (true)
+	{
+		printf("DEBUG: accepting incoming connections...\n");
+		
+		serverSocket = TCListenSocketAccept(listenSocket, listenTimeout);
+		
+		if (serverSocket != NULL)
+		{
+			printf("       connection accepted\n");
+		}
+		
+		TCServerSocketDestroy(serverSocket);
+	}
 	
 	return EXIT_SUCCESS;
+}
+
+void usage()
+{
+	printf("usage: server <listen port>\n");
 }
