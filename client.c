9f9ef86f14e230df9c18525e7e1a1f8c7507405d
Alex Rozenshteyn <rpglover64@gmail.com>
2010-04-04 16:15:35 -0400

    Add basic pthread usage.

--- client.c
+++ client.c
@@ -1,8 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #include "reader.h"
 #include "queue.h"
+
+struct reader_args
+{
+	int sock;
+	queue *q;
+};
+
+void *reader_wrap(void *argv)
+{
+	struct reader_args *args = (struct reader_args *) argv;
+	reader(args->sock, args->q);
+	return NULL;
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +28,12 @@ int main(int argc, char *argv[])
 	int sock = lookup(argv[1], argv[2]);
 	queue *q = init_queue();
 
-	reader(sock, q);
+	pthread_t reader_tid;
+	int status;
+	struct reader_args args = { .sock = sock, .q = q };
+	status = pthread_create(&reader_tid, NULL, reader_wrap, (void *) &args);
+	// FIXME check status
+	pthread_join(reader_tid, NULL);
 	
 	free_queue(q);
 	return EXIT_SUCCESS;
