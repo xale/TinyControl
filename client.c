fe004d704a68c3826dce42b6e82d488f456a9688
Alex Rozenshteyn <rpglover64@gmail.com>
2010-04-04 17:08:45 -0400

    Add writer loop.

--- client.c
+++ client.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #include "reader.h"
 #include "queue.h"
@@ -18,6 +20,40 @@ void *reader_wrap(void *argv)
 	return NULL;
 }
 
+int writer(queue* q)
+{
+	queue_elem *pqe;
+	ssize_t bytes_written;
+	while (q->count > 0)
+	{
+		pqe = pop_front(q);
+		bytes_written = write(STDOUT_FILENO, pqe->payload, pqe->payload_size);
+		// FIXME: check bytes_written
+		free(pqe);
+	}
+
+	return 0;
+}
+
+struct writer_args
+{
+	queue *q;
+	pthread_t reader_tid;
+};
+
+void *writer_wrap(void *argv)
+{
+	struct writer_args *args = (struct writer_args*) argv;
+	int success = -1;
+	while (success != 0)
+	{
+		writer(args->q);
+		success = pthread_tryjoin_np(args->reader_tid, NULL);
+		//fprintf(stderr, "Success in writer joining reader? %d.\n", success);
+	}
+	return NULL;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
@@ -28,12 +64,19 @@ int main(int argc, char *argv[])
 	int sock = lookup(argv[1], argv[2]);
 	queue *q = init_queue();
 
-	pthread_t reader_tid;
+	pthread_t reader_tid, writer_tid;
 	int status;
-	struct reader_args args = { .sock = sock, .q = q };
-	status = pthread_create(&reader_tid, NULL, reader_wrap, (void *) &args);
+	struct reader_args r_args = { .sock = sock, .q = q };
+	status = pthread_create(&reader_tid, NULL, reader_wrap, &r_args);
+	fprintf(stderr, "Status of creating reader: %d.\n", status);
 	// FIXME check status
-	pthread_join(reader_tid, NULL);
+
+	struct writer_args w_args = { .q = q, .reader_tid = reader_tid };
+	status = pthread_create(&writer_tid, NULL, writer_wrap, &w_args);
+	fprintf(stderr, "Status of creating writer: %d.\n", status);
+	// FIXME check status
+
+	pthread_join(writer_tid, NULL);
 	
 	free_queue(q);
 	return EXIT_SUCCESS;
