4150fed812a638c6891ba3400c6ce5f605aed2aa
Alex Rozenshteyn <rpglover64@gmail.com>
2010-04-04 20:25:41 -0400

    Use condition variables and avoid tryjoin.

--- client.c
+++ client.c
@@ -11,12 +11,14 @@ struct reader_args
 {
 	int sock;
 	queue *q;
+	int *finished;
 };
 
 void *reader_wrap(void *argv)
 {
 	struct reader_args *args = (struct reader_args *) argv;
 	reader(args->sock, args->q);
+	*args->finished = 1;
 	return NULL;
 }
 
@@ -41,17 +43,15 @@ struct writer_args
 {
 	queue *q;
 	pthread_t reader_tid;
+	int *reader_finished;
 };
 
 void *writer_wrap(void *argv)
 {
 	struct writer_args *args = (struct writer_args*) argv;
-	int success = -1;
-	while (success != 0)
+	while (*args->reader_finished == 0)
 	{
 		writer(args->q);
-		success = pthread_tryjoin_np(args->reader_tid, NULL);
-		//fprintf(stderr, "Success in writer joining reader? %d.\n", success);
 	}
 	return NULL;
 }
@@ -63,17 +63,18 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: %s hostname port\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+	int finished = 0;
 	int sock = lookup(argv[1], argv[2]);
 	queue *q = init_queue();
 
 	pthread_t reader_tid, writer_tid;
 	int status;
-	struct reader_args r_args = { .sock = sock, .q = q };
+	struct reader_args r_args = { .sock = sock, .q = q , .finished = &finished };
 	status = pthread_create(&reader_tid, NULL, reader_wrap, &r_args);
 	fprintf(stderr, "Status of creating reader: %d.\n", status);
 	// FIXME check status
 
-	struct writer_args w_args = { .q = q, .reader_tid = reader_tid };
+	struct writer_args w_args = { .q = q, .reader_tid = reader_tid, .reader_finished = &finished };
 	status = pthread_create(&writer_tid, NULL, writer_wrap, &w_args);
 	fprintf(stderr, "Status of creating writer: %d.\n", status);
 	// FIXME check status
--- queue.c
+++ queue.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <assert.h>
+#include <pthread.h>
 
 #include "queue.h"
 
@@ -16,6 +17,8 @@ queue* init_queue(void)
 	ret->head = NULL;
 	ret->tail = NULL;
 	ret->count = 0;
+	pthread_mutex_init(&ret->empty_lock, NULL);
+	pthread_cond_init(&ret->empty, NULL);
 	return ret;
 }
 
@@ -48,6 +51,9 @@ int push_back(queue* q, unsigned int payload_size, payload_t payload)
 		q->tail = last;
 	}
 	q->count++;
+	pthread_mutex_lock(&q->empty_lock);
+	pthread_cond_signal(&q->empty);
+	pthread_mutex_unlock(&q->empty_lock);
 	return 0;
 }
 
